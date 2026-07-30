7f4a46f46f9e0ff57a76584e03cb6acd6be45bac
Jacky <jackylai1030@gmail.com>
2026-07-30 11:56:53 -0400

    started the handleSubmit logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import { useEffect } from "react"
+import { useRef } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
-
   const [todos, setTodos] = useState([]);
+  const inputRef = useRef();
 
   async function getData() {
-    const response = await fetch('http://localhost:3000/api/todos');
+    const response = await fetch("http://localhost:3000/api/todos");
     const data = await response.json();
-    console.log(data)
+    console.log(data);
     setTodos(data);
   }
 
@@ -15,17 +16,36 @@ export default function App() {
     getData();
   }, []);
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // package up our todo with the input value
+    const todo = {
+      text: inputRef.current.value,
+    };
+
+    console.log(todo);
+
+    // send this data as a POST request 
+  }
+
   return (
     <div>
       <h1>Todos</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef} />
+        <button>Submit</button>
+      </form>
+
       <ul>
-        {todos.map((todo) => 
-        <li key={todo._id}>
-          <input type="checkbox" checked={todo.completed}/>
-          {todo.text}
-        </li>
-        )}
+        {todos.map((todo) => (
+          <li key={todo._id}>
+            <input type="checkbox" checked={todo.completed} />
+            {todo.text}
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
