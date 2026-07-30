3f2d23a51f594bbf19c427338eb725c4a8bbb485
Jacky <jackylai1030@gmail.com>
2026-07-30 11:31:48 -0400

    added some state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import { useEffect } from "react"
 
 export default function App() {
 
-  async function test() {
-    const response = await fetch('http://localhost:3000/test');
+  const [todos, setTodos] = useState([]);
+
+  async function getData() {
+    const response = await fetch('http://localhost:3000/api/todos');
     const data = await response.json();
-    console.log(data);
+    console.log(data)
+    setTodos(data);
   }
 
   useEffect(() => {
-    test();
+    getData();
   }, []);
 
   return (
