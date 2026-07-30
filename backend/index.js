fb9f7123b8bfbebc19f3e51b6e212fa02a6ada56
Jacky <jackylai1030@gmail.com>
2026-07-30 13:22:17 -0400

    added a post route

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import connectDB from './db.js';
+import connectDB from "./db.js";
 
-import Todo from './models/todo.js'
+import Todo from "./models/todo.js";
 
 const app = express();
 
@@ -14,12 +14,23 @@ const port = 3000;
 // allow requests from frontend
 app.use(cors());
 
-app.get('/api/todos', (req, res) => {
-    const todos = await.Todo.find();
-    res.json(todos);
-})
+// format incoming data to json
+app.use(express.json());
+
+// GET todos
+app.get("/api/todos", async (req, res) => {
+  const todos = await Todo.find({});
+  res.json(todos);
+});
+
+// POST create a todo
+app.post("/api/todos", async (req, res) => {
+    console.log(req.body);
+    const todo = await Todo.create(req.body);
+    res.json(todo);
+});
 
 app.listen(port, () => {
-    console.log('Listening on port: ', port);
-    connectDB();
-})
+  console.log("Listening on port: ", port);
+  connectDB();
+});
